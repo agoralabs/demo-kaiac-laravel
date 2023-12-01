b3e45f6d314b73b211eb6b88f3584a988fd8956c
Joseph Stephane EKOBO KIDOU <agora.labs.contact@gmail.com>
2023-12-01 02:44:27 +0000

    Refactoring Devops

--- devops/buildspec.sh
+++ devops/buildspec.sh
@@ -2,21 +2,19 @@
 
 THE_DATE=$(date '+%Y-%m-%d %H:%M:%S')
 echo "Build started on $THE_DATE"
-echo $PWD
-echo $0
 
 if [ "$TF_VAR_ENV_APP_GL_SCRIPT_MODE" == "CLOUDOCKER" ] 
 then
 
-    . build_cloud_docker.sh
+    . ./devops/build_cloud_docker.sh
 
 elif [ "$TF_VAR_ENV_APP_GL_SCRIPT_MODE" == "CLOUDEKS" ] 
 then
  
-    . build_cloud_eks.sh
+    . ./devops/build_cloud_eks.sh
 
 else
 
-    . build_cloud.sh
+    . ./devops/build_cloud.sh
 
 fi
